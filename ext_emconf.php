c7f2bc69a8575821dc2cb77fa2dd754f76ec1846
Danny Stey <mail@danny-stey.de>
2022-08-04 17:06:03 +0200

    new support

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,9 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.0.0-10.99.99,11.5.0-11.5.99',
+            'typo3' => '10.4.0-11.5.99',
         ],
-        'conflicts' => [],
-        'suggests' => [],
     ],
 ];
