e2683891c0e30c87998095a47e8db33c7f2512f2
Oliver Wagner <ow@pixx.io>
2025-12-09 20:02:53 +0100

    Ändere Bezeichnung von 'pixx.io Direct link' zu 'pixx.io CDN link' und passe Standardwert für 'pixxio_direct_link' an

--- ext_tables.sql
+++ ext_tables.sql
@@ -4,7 +4,7 @@ CREATE TABLE sys_file_metadata (
     pixxio_downloadformat varchar(255) NOT NULL DEFAULT '',
     pixxio_last_sync_stamp int(11) NOT NULL DEFAULT '0',
     pixxio_is_direct_link int(1) NOT NULL DEFAULT '0',
-    pixxio_direct_link varchar(255) NOT NULL DEFAULT '0',
+    pixxio_direct_link varchar(255) NOT NULL DEFAULT '',
     tx_pixxioextension_licensereleases text
 );
 
