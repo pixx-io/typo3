4798518d211ed2c53c8549a018bd608f2471ca7a
Oliver Wagner <ow@pixx.io>
2026-05-29 16:59:09 +0200

    feat: implement single file sync functionality with options for TYPO3 UID and pixx.io ID

--- Classes/Command/SyncCommand.php
+++ Classes/Command/SyncCommand.php
@@ -5,6 +5,7 @@ namespace Pixxio\PixxioExtension\Command;
 use Pixxio\PixxioExtension\Controller\FilesController;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -16,7 +17,21 @@ class SyncCommand extends Command
    */
   protected function configure()
   {
-    $this->setHelp('Synchronizes files from Pixxio.' . "\n" . 'Use this command to sync media assets from your pixx.io mediaspace.');
+    $this->setHelp('Synchronizes files from Pixxio.' . "\n" . 'Use this command to sync media assets from your pixx.io mediaspace.' . "\n" . 'Options:' . "\n" . '  --fid=<id>  Sync a specific file by TYPO3 file UID' . "\n" . '  --pid=<id>  Sync a specific file by pixx.io ID');
+    
+    $this->addOption(
+      'fid',
+      null,
+      InputOption::VALUE_REQUIRED,
+      'TYPO3 file UID to sync'
+    );
+    
+    $this->addOption(
+      'pid',
+      null,
+      InputOption::VALUE_REQUIRED,
+      'pixx.io file ID to sync'
+    );
   }
 
   protected function execute(InputInterface $input, OutputInterface $output): int
@@ -25,10 +40,27 @@ class SyncCommand extends Command
 
       $io->title($this->getDescription());
 
+      $fid = $input->getOption('fid');
+      $pid = $input->getOption('pid');
+      
+      // Check if both options are provided
+      if ($fid && $pid) {
+          $io->error('Please provide either --fid or --pid, not both');
+          return Command::INVALID;
+      }
+
       $io->writeln('🚀 Start syncing');
       try {
           $filesController = GeneralUtility::makeInstance(FilesController::class);
-          $result = $filesController->syncAction($io);
+          
+          // Sync single file if fid or pid is provided
+          if ($fid || $pid) {
+              $result = $filesController->syncSingleFileAction($io, $fid, $pid);
+          } else {
+              // Sync all files
+              $result = $filesController->syncAction($io);
+          }
+          
           if ($result) {
               $io->success('🪐 synchronization successful');
               return Command::SUCCESS;
